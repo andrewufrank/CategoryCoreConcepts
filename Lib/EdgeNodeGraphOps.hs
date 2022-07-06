024ff296fd4960086b969d14c3d962f9bf0e4d42
Andrew U Frank <frank@geoinfo.tuwien.ac.at>
2022-07-06 20:52:41 +0200

    added the checks for fun resp rel

--- Lib/EdgeNodeGraphOps.hs
+++ Lib/EdgeNodeGraphOps.hs
@@ -99,19 +99,61 @@ find_node_edge2 i = do
         r1  <- find  (Just . NodeTag $ i, Nothing, Nothing) 
         return . getTarget3  $ r1
 
+
 sFun :: () =>  Node -> StoreStateMonad  (Edge)
 -- ^ start with node get edge
 -- get the s related edge, fails on relation
 sFun i = do 
         r1  <- find  (Just . NodeTag $ i, Just sMorph, Nothing) 
-        return . unEdgeTag . getTarget3  $ r1
-tInv :: () =>  Edge -> StoreStateMonad  Node
+        return . unEdgeTag . getSingle3  $ r1
+sRel :: () =>  Node -> StoreStateMonad  ([Edge])
+-- ^ start with node get edge
+-- get the s related edge, fails on relation
+sRel i = do 
+        r1  <- find  (Just . NodeTag $ i, Just sMorph, Nothing) 
+        return . map unEdgeTag . map trd3  $ r1
+tFun :: () =>  Node -> StoreStateMonad  (Edge)
+-- ^ start with node get edge using t
+tFun i = do 
+        r1  <- find  (Just . NodeTag $ i, Just tMorph, Nothing) 
+        return . unEdgeTag . getSingle3  $ r1
+tRel :: () =>  Node -> StoreStateMonad  ([Edge])
+-- ^ start with node get edge using t
+tRel i = do 
+        r1  <- find  (Just . NodeTag $ i, Just tMorph, Nothing) 
+        return . map unEdgeTag . map trd3  $ r1
+
+
+
+sInvFun :: () =>  Edge -> StoreStateMonad  Node
+-- ^ start with edge get node using s
+sInvFun i = do 
+        r1  <- find  (Nothing, Just sMorph, Just . EdgeTag $ i) 
+        return . unNodeTag . getSingle1  $ r1
+sInvRel :: () =>  Edge -> StoreStateMonad  [Node]
+-- ^ start with edge get node using s
+sInvRel i = do 
+        r1  <- find  (Nothing, Just sMorph, Just . EdgeTag $ i) 
+        return . map unNodeTag . map fst3  $ r1
+tInvFun :: () =>  Edge -> StoreStateMonad  Node
 -- ^ start with edge get node using t 
-tInv i = do 
+tInvFun i = do 
         r1  <- find  (Nothing, Just tMorph, Just . EdgeTag $ i) 
-        return . unNodeTag . getTarget1  $ r1
+        return . unNodeTag . getSingle1  $ r1
+tInvRel :: () =>  Edge -> StoreStateMonad  [(Node)]
+-- ^ start with edge get node using t 
+tInvRel i = do 
+        r1  <- find  (Nothing, Just tMorph, Just . EdgeTag $ i) 
+        return . map unNodeTag . map fst3  $ r1
 
 
+isSingleton a = 1 == length a
+openSingleton [a] = a
+openSingleton x = errorT ["openSingleton - not", showT x ]
+
+getSingle1 = fst3 . openSingleton
+-- ^ unwrap the first if singleton 
+getSingle3 = trd3 . openSingleton
 
 
 part2 :: ErrIO ()
@@ -130,11 +172,15 @@ part2 = do
     putIOwords  ["the result of node edge query", showT res3]
     let res4 = evalState  (sFun (Node 1)) nt1
     putIOwords  ["the result of node edge query", showT res4]
-    let res5 = evalState  (tInv (Edge 'e')) nt1
+    let res5 = evalState  (tInvFun (Edge 'e')) nt1
     putIOwords  ["the result of edge node t query", showT res5]
-    let res6 = evalState  (tInv =<< sFun (Node 1)) nt1
+    let res6 = evalState  (tInvFun =<< sFun (Node 1)) nt1
     -- to make it like . composition (otherwse use >>= )
     putIOwords  ["the result of node - s - tinv - node", showT res6]
+    putIOwords  ["the result of tinv 'e'  edge"
+            , showT $ evalState (tInvRel (Edge 'e')) nt1]
+    putIOwords  ["the result of edge e - tInv - node - s - edge"
+            , showT $ evalState (sFun =<< tInvFun (Edge 'e')) nt1]
 
 -- type MorphST  = Either S T  
 -- -- ^ the morphism from NodeTag to EdgeTag 
