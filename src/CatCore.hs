6723a59c9ef23632702d522c4e27624999539af7
Andrew U Frank <frank@geoinfo.tuwien.ac.at>
2022-07-12 13:00:06 +0200

    cleaned up

--- src/CatCore.hs
+++ src/CatCore.hs
@@ -14,33 +14,6 @@
 
 module Main     where      -- must have Main (main) or Main where
 
- 
--- import           Lib.DirTree
--- import  Lib.Page13
--- import  Lib.Page39
--- import  Lib.Page86
--- -- import  Lib.Page135
--- import  Lib.Page135hask
--- import Lib.HaskGraph
--- import Lib.CCworld 
--- import Lib.CCdata
--- import Lib.ExampleSet
--- import Lib.ExampleData
--- import Lib.Rules
--- import Lib.UsingSets
--- import           Lib.OpenClass
--- import Vault.Tris
--- import Lib.STproduct ()
--- import Vault.NaiveTripleStore
--- import Vault.Triple4cat
--- import Lib.STproductTriple
--- -- import Lib.STproductCombine
--- import Lib.DataInState
--- import Vault.TrisFiles 
--- import Lib.EdgeNodeGraph
--- import Lib.EdgeNodeGraphOps
--- import Lib.EdgeNodeDistance 
--- import Lib.Point
 import UniformBase
 import GIS.Category
 import GIS.Store
@@ -49,25 +22,11 @@ import GIS.Store_data
 main :: IO ()
 main =  do  -- with tests in other modules
 
-    -- pageCCdata
-    -- pageTriple4cat
-    -- pageST
-    -- pageST_withTriples
-    -- pageST_withTriplesBusiness
-    -- pageSTproductCombines
-    -- pageSTproductCombines2
-    -- pageNT
-
     startProg "CatCore" mainErrIO 
 
 mainErrIO :: ErrIO ()
 mainErrIO = do 
-    -- pageTris
-    -- pageDataInState
-    -- pageEdgeNodeGraph
-    -- part2
-    -- pageEdgeNodeDistance
-    -- pagePoint
+
     pageStore
 
 
